dd1abb1d214ed351da7e2642bb36c47e03652cfc
Hackerly0 <hnisirat@gmail.com>
2025-10-05 20:01:34 +0300

    idk what to right, but this the function that grabs the questions and answers

---
+++ src/grab_questions.c
@@ -0,0 +1,144 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include "libft.h"
+#include "../GNL/get_next_line.h"
+
+static int	is_blank_line(const char *s)
+{
+	size_t	i;
+
+	if (!s)
+		return (1);
+	i = 0;
+	while (s[i] == ' ' || s[i] == '\t' || s[i] == '\r' || s[i] == '\n')
+		i++;
+	if (s[i] == '\0')
+		return (1);
+	return (0);
+}
+
+static void	rtrim(char *s)
+{
+	size_t	n;
+
+	if (!s)
+		return ;
+	n = ft_strlen(s);
+	while (n > 0 && (s[n - 1] == '\n' || s[n - 1] == '\r'
+			|| s[n - 1] == ' ' || s[n - 1] == '\t'))
+	{
+		n--;
+		s[n] = '\0';
+	}
+}
+
+static void	trim_fields(char **qa)
+{
+	int		i;
+	size_t	n;
+
+	if (!qa)
+		return ;
+	i = 0;
+	while (qa[i])
+	{
+		n = ft_strlen(qa[i]);
+		while (n > 0 && (qa[i][n - 1] == '\n' || qa[i][n - 1] == '\r'
+				|| qa[i][n - 1] == ' ' || qa[i][n - 1] == '\t'))
+		{
+			n--;
+			qa[i][n] = '\0';
+		}
+		i++;
+	}
+}
+
+static int	count_nonblank(const char *path)
+{
+	int		fd;
+	int		count;
+	char	*line;
+
+	fd = open(path, O_RDONLY);
+	if (fd < 0)
+		return (-1);
+	count = 0;
+	line = NULL;
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (!line)
+			break ;
+		if (!is_blank_line(line))
+			count++;
+		free(line);
+	}
+	close(fd);
+	return (count);
+}
+
+static char	*get_kth_nonblank(const char *path, int k)
+{
+	int		fd;
+	int		idx;
+	char	*line;
+
+	fd = open(path, O_RDONLY);
+	if (fd < 0)
+		return (NULL);
+	idx = 0;
+	line = NULL;
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (!line)
+			break ;
+		if (!is_blank_line(line))
+		{
+			if (idx == k)
+				break ;
+			idx++;
+		}
+		free(line);
+	}
+	close(fd);
+	return (line);
+}
+
+static void	seed_once(void)
+{
+	static int	seeded;
+
+	if (!seeded)
+	{
+		srand((unsigned int)time(NULL));
+		seeded = 1;
+	}
+}
+
+char	**grab_questions(void)
+{
+	const char	*path = "questions";
+	int			total;
+	int			target;
+	char		*line;
+	char		**qa;
+
+	total = count_nonblank(path);
+	if (total <= 0)
+		return (NULL);
+	seed_once();
+	target = rand() % total;
+	line = get_kth_nonblank(path, target);
+	if (!line)
+		return (NULL);
+	rtrim(line);
+	qa = ft_split(line, ',');
+	free(line);
+	if (!qa)
+		return (NULL);
+	trim_fields(qa);
+	return (qa);
+}
